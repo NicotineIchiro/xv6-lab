b20f3b2fc15437213b55627ea0221e6c6bb3d50b
HY <h_rer@foxmail.com>
2024-03-15 22:03:36 +0800

    Before checkout to traps

--- kernel/vm.c
+++ kernel/vm.c
@@ -39,7 +39,7 @@ kvmmake(void)
   // map kernel data and the physical RAM we'll make use of.
   kvmmap(kpgtbl, (uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);
 
-	kvmmap(kpgtbl, USYSCALL, (uint64)trampoline - PGSIZE, PGSIZE, PTE_R);
+	//kvmmap(kpgtbl, USYSCALL, (uint64)trampoline - PGSIZE, PGSIZE, PTE_R);
 
   // map the trampoline for trap entry/exit to
   // the highest virtual address in the kernel.
